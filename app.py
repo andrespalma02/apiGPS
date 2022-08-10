550c07fa1ef0d674062c7dd8a84922d1d8bda801
Andrés Palma <bryan.palma02@epn.edu.ec>
2022-08-10 17:09:47 -0500

    funcionando

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from flask import Flask, request
 import requests
+from flask import Flask, request as rq
 
 app = Flask(__name__)
 apiKey = "3809ccdc104d405f5387031f81abcf9cadba6f1767fcdff3"
 headers = {"Content-Type": "application/json",
            "X-Api-Key": apiKey,
            "X-Username": "bryan.palma02@epn.edu.ec"}
+url = f"https://app.flokzu.com/flokzuopenapi/api/{apiKey}/database/"
 
 
-@app.route('/inventario_insumos', methods=['POST'])
+@app.route('/')
+def hello():
+    return "Hola mundo"
+
+
+@app.route('/inventario_insumos/', methods=['POST'])
 def hello_world():
-    json_data = request.form
-    tabla=json_data["tabla"]
-    for item in tabla:
-        requests.put(f"https://app.flokzu.com/flokzuopenapi/api/{apiKey}/database"
-                     "/PRO_Inventario_Insumos_Materiales", json_data, "", headers)
-    return 0
+    json_data = rq.json
+    for items in json_data["tabla"]:
+        r = requests.put(url + "Inventario_Desperdicios", data=items, headers=headers)
+        print(r.status_code)
+    return "listo"
 
 
 if __name__ == '__main__':
