67155e473aa4192c7749ef33a472d4fd7328ae8e
Andrés Palma <bryan.palma02@epn.edu.ec>
2022-08-13 18:19:03 -0500

    cambios

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ apiKey = "3809ccdc104d405f5387031f81abcf9cadba6f1767fcdff3"
 headers = {"Content-Type": "application/json",
            "X-Api-Key": apiKey,
            "X-Username": "bryan.palma02@epn.edu.ec"}
-url = f"https://app.flokzu.com/flokzuopenapi/api/{apiKey}/database/Inventario_Desperdicios"
+url = f"https://app.flokzu.com/flokzuopenapi/api/{apiKey}/database/"
 
 
 @app.route('/')
@@ -19,9 +19,22 @@ def hello_world():
     status = []
     json_data = rq.json
     for items in json_data["tabla"]:
-        r = requests.put(url, json=items, headers=headers)
+        r = requests.put(url + "Inventario_Desperdicios", json=items, headers=headers)
         status.append(r.text)
     return status.__str__()
+
+
+@app.route('/inventario_recepcion/', methods=['POST'])
+def recepcion():
+    pavos = {}
+    json_data = rq.json
+    for items in json_data["tabla"]:
+        pavos[items["Nombre"]] = int(items["Cantidad"])
+    pavos["Fecha de Recepción"] = json_data["Fecha de Recepción"]
+    print(pavos)
+    r = requests.put(url + "inventario_de_recepcion", json=pavos, headers=headers)
+    print(url + "inventario_de_recepcion")
+    return r.text.__str__()
 
 
 @app.route('/inventario_insumos', methods=['GET'])
